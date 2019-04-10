0b8ebb45fef7d9b665c0a70fe1c3a246a78c1d4d
RENISHAW\fb142897 <freddie.burns@renishaw.com>
2019-04-10 11:49:53 +0100

    Created DataServiceClass.
    Added Data folder to UI with FriendDataService class. Added function to yield friends. Hard coded values for now, to be replaced by database access in future.

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -2,5 +2,12 @@
 {
     public class Friend
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
     }
 }
