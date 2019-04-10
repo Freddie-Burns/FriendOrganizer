aeb8ecd8478bc71ca0f7a3f0dde03d7cc1a61663
RENISHAW\fb142897 <freddie.burns@renishaw.com>
2019-04-10 14:26:28 +0100

    Moved the OnPropertyChanged method to new class.
    Made the method protected virtual so it can be used by sub classes and overriden. Now MainViewModel inherits from BaseViewModel rather than directly implementing the INotifyPropertyChanged interface.

--- FriendOrganizer.UI/ViewModel/MainViewModel.cs
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -6,12 +6,10 @@ using System.Runtime.CompilerServices;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : ViewModelBase
     {
         private IFriendDataService _friendDataService;
         private Friend _selectedFriend;
-
-        public event PropertyChangedEventHandler PropertyChanged;
 
         public MainViewModel(IFriendDataService friendDataService)
         {
@@ -40,11 +38,15 @@ namespace FriendOrganizer.UI.ViewModel
                 OnPropertyChanged();
             }
         }
+    }
 
-        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
     }
 }
