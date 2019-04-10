e871b0306c64e62a1d04ffb165fb4d668b57ff70
RENISHAW\fb142897 <freddie.burns@renishaw.com>
2019-04-10 11:51:11 +0100

    Extracted interface from FriendDataService class.
    Just used the ctrl + . intellisense option in VS.

--- FriendOrganizer.UI/Data/FriendDataService.cs
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace FriendOrganizer.UI.Data
 {
-    class FriendDataService
+    class FriendDataService : IFriendDataService
     {
         public IEnumerable<Friend> GetAll()
         {
