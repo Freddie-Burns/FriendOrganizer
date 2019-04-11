47723188e832dc755d67e04b086d064c02934477
RENISHAW\fb142897 <freddie.burns@renishaw.com>
2019-04-11 08:48:50 +0100

    Added first Code First Migration.
    Updated FriendOrganizer.DataAccess\App.config to have a connection string of FriendOrganizerDb which is the string we used when calling the DbContext base constructor in FriendOrganizerDbContext. In the nuget package manager console we used "Enable-Migrations" for DataAccess. We added a seed method to the generated Configuration.cs file in Migrations to add or update the database with hard coded values. Then we called "Add-Migration InitialDatabase" with the latter being the name of the created database. This created a new database cs file in the Migrations folder.

--- FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FriendOrganizer.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace FriendOrganizer.DataAccess
 {
-    class FriendOrganizerDbContezt
+    public class FriendOrganizerDbContext : DbContext
     {
+        public FriendOrganizerDbContext() : base("FriendOrganizerDb")
+        {
+             
+        }
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
